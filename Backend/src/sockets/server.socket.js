d287df647fc0383a275bd95802d970c7e46c50df
Sanket Vishwakarma <your-email@example.com>
2026-07-03 20:10:54 +0530

    Update socket.io initialization to use correct httpServer reference and modify CORS origins

--- Backend/src/sockets/server.socket.js
+++ Backend/src/sockets/server.socket.js
@@ -3,9 +3,12 @@ import { Server } from "socket.io";
 let io;
 
 export function initSocket(httpServer) {
-  io = new Server(httpServer, {
+  const io = new Server(server, {
     cors: {
-      origin: "http://localhost:5173",
+      origin: [
+        "http://localhost:5173",
+        "https://perplexity-1-5gaq.onrender.com",
+      ],
       credentials: true,
     },
   });
